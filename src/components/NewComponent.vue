e88b31604f3b85e67eeb7fc848ddb141944162f7
Charygulyyev <99390211+Charygulyyev@users.noreply.github.com>
2022-03-18 23:22:36 +0300

    commit-1

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="vuetify">
+    <v-container grid-list-xs12>
+      <v-btn color="info">text</v-btn>
+    </v-container>
+    <v-content>
+      <h2>men uly</h2>
+    </v-content>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass">
+@import ../assets/sass/main
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <new-component />
   </div>
 </template>
+<script>
+import NewComponent from "@/components/NewComponent.vue";
+export default {
+  name: "AboutView",
+  components: { NewComponent },
+};
+</script>
